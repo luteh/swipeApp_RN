2aa9b61ca929722a6ea5b8e46cb532c82731591a
luteh <luthfanmaftuh@gmail.com>
2017-06-20 01:34:05 +0700

    Component boilerplate

---
+++ src/Deck.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Luteh on 20/06/2017.
+ */
+import React, {Component} from 'react'
+import {View, Animated} from 'react-native'
+
+class Deck extends Component {
+    render() {
+        return;
+    }
+}
+
+export default Deck
