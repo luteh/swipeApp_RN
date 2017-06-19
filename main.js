32c052591a5798e3d1568202f30ea0846f83f3fe
luteh <luthfanmaftuh@gmail.com>
2017-06-20 01:38:38 +0700

    Changed deck data code position

--- main.js
+++ main.js
@@ -3,16 +3,6 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Deck from './src/Deck';
 
-class App extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Deck/>
-            </View>
-        );
-    }
-}
-
 const DATA = [
     { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
     { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
@@ -23,6 +13,16 @@ const DATA = [
     { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
     { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
 ];
+
+class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Deck/>
+            </View>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     container: {
