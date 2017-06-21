0e3cd2875e94cf0512af9216013a02afcb2a1028
luteh <luthfanmaftuh@gmail.com>
2017-06-21 10:48:15 +0700

    The cards be able to swiping now

--- src/Deck.js
+++ src/Deck.js
@@ -11,16 +11,18 @@ import {
 class Deck extends Component {
     constructor(props) {
         super(props);
+        const position = new Animated.ValueXY();
         const panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: (event, gesture) => {
-                console.log(gesture);
+                position.setValue({x: gesture.dx, y: gesture.dy});
             },
             onPanResponderRelease: () => {
             }
 
         });
-        this.state = {panResponder};
+        this.position = position;
+        this.state = {panResponder, position};
     }
 
     renderCards() {
@@ -31,9 +33,12 @@ class Deck extends Component {
 
     render() {
         return (
-            <View>
+            <Animated.View
+                style={this.state.position.getLayout()}
+                {...this.state.panResponder.panHandlers}
+            >
                 {this.renderCards()}
-            </View>
+            </Animated.View>
         )
     };
 }
