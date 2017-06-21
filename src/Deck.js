fb6f7cf58ce4644d68a4618772ce7198dee3ae3f
luteh <luthfanmaftuh@gmail.com>
2017-06-21 16:39:31 +0700

    cascading card list

--- src/Deck.js
+++ src/Deck.js
@@ -119,6 +119,7 @@ class Deck extends Component {
                 //add animated to this view to handle the flashing image
                 <Animated.View
                     key={item.id}
+                    // top: 10 * (i - this.state.index) < this style is for cascading card list
                     style={[styles.cardStyle, {top: 10 * (i - this.state.index)}]}>
                     {this.props.renderCard(item)}
                 </Animated.View>
