633af2d2ebf87d49037a7b78c2a2a7a38933e944
luteh <luthfanmaftuh@gmail.com>
2017-06-21 11:49:10 +0700

    Added Dimension, resetPosition helper method, and refactor getCardStyle method

--- src/Deck.js
+++ src/Deck.js
@@ -5,8 +5,11 @@ import React, {Component} from 'react'
 import {
     View,
     Animated,
-    PanResponder
+    PanResponder,
+    Dimensions
 } from 'react-native'
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
     constructor(props) {
@@ -17,18 +20,32 @@ class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({x: gesture.dx, y: gesture.dy});
             },
+            //this function will call every user stopped swipe
             onPanResponderRelease: () => {
+                this.resetPosition();
             }
 
         });
-        this.position = position;
         this.state = {panResponder, position};
     }
 
-    getCardStyle(){
-        return{
-            ...this.state.position.getLayout(),
-            transform: [{rotate:'45deg'}]
+    //Springing the card back to default
+    resetPosition() {
+        Animated.spring(this.state.position, {
+            toValue: {x: 0, y: 0}
+        }).start();
+    }
+
+    getCardStyle() {
+        const {position} = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.2, 0, SCREEN_WIDTH * 1.2],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+        return {
+            ...position.getLayout(),
+            transform: [{rotate: rotate}]
         }
     }
 
